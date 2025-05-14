ca4838813854042834450be7f13616700277f395
Ibrahin20 <ibrahinpacheco123@gmail.com>
2025-05-14 19:24:47 -0400

    update

---
+++ Ejercicios/ejercicio.js
@@ -0,0 +1,9 @@
+let numero = 3;
+if (numero % 2 === 0) {
+    console.log("Numero par");
+} 
+else  {
+    console.log("NUmero impar");
+}
+
+
---
+++ Ejercicios/script.js
@@ -0,0 +1,3 @@
+let nombre = ("Ibrahin Pacheco")  
+const cd = 3153041;
+var edad = 18;
